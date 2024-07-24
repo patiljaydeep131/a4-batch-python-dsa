347f4065ec378eda7c499bcf50474de05c9ccdf2
patiljaydeep131 <37292319+patiljaydeep131@users.noreply.github.com>
2024-07-25 02:11:47 +0530

    Update q9.py

--- Solutions/Week 4/Day 3 Assignment/q9.py
+++ Solutions/Week 4/Day 3 Assignment/q9.py
@@ -10,3 +10,10 @@ def is_prime_number(num: int) -> bool:
 n = int(input("Enter n = "))
 my_list = [i for i in range(2, n + 1) if is_prime_number(i)]
 print(my_list)
+
+
+#Solution2_jaydeep
+l=int(input("enter the number divisible by 5:"))
+lst=[i for i in range(1,l+1) if i%5==0 ]
+#n=len(lst)
+print(lst) 
